9fdaf190469cfa50799ca036b7771d2f2e467f9b
godoing <20141105063T@mac04.local>
2015-11-18 20:51:16 +0800

    light
    控制灯光闪烁间隔

---
+++ _123.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ digitalWrite(13,HIGH);
+  delay(250);
+  digitalWrite(13,LOW);
+  delay(250);
+  digitalWrite(13,HIGH);
+  delay(50);
+  digitalWrite(13,LOW);
+  delay(50);
+}
